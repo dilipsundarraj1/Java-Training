f2ac6e12f9327e7a3b04837accfb835d9966e623
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2017-12-21 13:32:43 -0600

    added the chapter 4 test 3

--- chapter-4/src/com/learnJava/accessModifiers/DefaultAccessModifier.java
+++ chapter-4/src/com/learnJava/accessModifiers/DefaultAccessModifier.java
@@ -1,8 +1,6 @@
 package com.learnJava.accessModifiers;
 
-/**
- * Created by z001qgd on 12/21/17.
- */
+
 class DefaultAccessModifier {
 
     int age;
---
+++ chapter-4/src/com/learnJava/anatomyClass/Sample.java
@@ -0,0 +1,13 @@
+package com.learnJava.anatomyClass;
+
+import com.learnJava.accessModifiers.PrivateAccessModifier;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        /*DefaultAccessModifier defaultAccessModifier = new DefaultAccessModifier()*/
+
+        PrivateAccessModifier privateAccessModifier= new PrivateAccessModifier();
+        privateAccessModifier.age = 12;
+    }
+}

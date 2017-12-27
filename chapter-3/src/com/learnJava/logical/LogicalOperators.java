25e5c4230f175c62c27dc915de0643051dc3ca4c
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2017-12-27 06:19:34 -0600

    added the logical examples and test

---
+++ chapter-3/src/com/learnJava/logical/LogicalOperators.java
@@ -0,0 +1,18 @@
+package com.learnJava.logical;
+
+/**
+ * Created by z001qgd on 12/27/17.
+ */
+public class LogicalOperators {
+
+    public static void main(String[] args) {
+
+        boolean a,b,c;
+
+        a=true;
+        b=false;
+        c=false;
+
+        System.out.println(a||b|| c);
+    }
+}

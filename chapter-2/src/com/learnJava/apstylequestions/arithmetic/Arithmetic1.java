f5796c051270baca699fbc4fd574e4ca1511bad7
Dilip.Sundarraj <Dilip.Sundarraj@target.com>
2018-01-01 10:00:09 -0600

    added the tests

---
+++ chapter-2/src/com/learnJava/apstylequestions/arithmetic/Arithmetic1.java
@@ -0,0 +1,24 @@
+package com.learnJava.apstylequestions.arithmetic;
+
+/**
+ * Created by z001qgd on 1/1/18.
+ */
+public class Arithmetic1 {
+
+    public static void main(String[] args) {
+
+        int a=5, b= 4;
+
+        double x=5.0,y=2.0;
+
+        System.out.println( (a*b) + x/y);
+
+        System.out.println( (a-b) + a + b * (a+b));
+
+        System.out.println( x + a);
+
+        System.out.println( a + b + x + y);
+
+        System.out.println( (a - b )/2);
+    }
+}
---
+++ chapter-2/src/com/learnJava/apstylequestions/arithmetic/Arithmetic2.java
@@ -0,0 +1,20 @@
+package com.learnJava.apstylequestions.arithmetic;
+
+/**
+ * Created by z001qgd on 1/1/18.
+ */
+public class Arithmetic2 {
+
+    public static void main(String[] args) {
+        int a=10, b= 4;
+
+        float x=10.0f,y=2.0f;
+
+
+        System.out.println((x+y * (a+b)));
+
+        System.out.println(((x+y) * (a+b)));
+
+        System.out.println(((x*y) +y) * (a+b));
+    }
+}
